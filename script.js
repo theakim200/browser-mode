edb43d473141432e41cd1cde93b14268b1b88446
theakim <theakim200@gmail.com>
2025-10-08 05:36:01 -0400

    Firefox

--- script.js
+++ script.js
@@ -4,26 +4,35 @@ console.log("Hi");
 function detectBrowser() {
     const userAgent = navigator.userAgent;
     
+    // 디버깅: userAgent 전체 출력
+    console.log("Full userAgent:", userAgent);
+    console.log("Contains Firefox?", userAgent.indexOf("Firefox") > -1);
+    console.log("Contains Chrome?", userAgent.indexOf("Chrome") > -1);
+    console.log("Contains Safari?", userAgent.indexOf("Safari") > -1);
+    
     // Firefox 먼저 체크
     if (userAgent.indexOf("Firefox") > -1) {
+        console.log("Detected: Firefox");
         return "firefox";
     }
     // Chrome 체크 (Edge 제외)
     else if (userAgent.indexOf("Chrome") > -1 && userAgent.indexOf("Edg") === -1) {
+        console.log("Detected: Chrome");
         return "chrome";
     }
     // Safari 체크 (Chrome이 없을 때만)
     else if (userAgent.indexOf("Safari") > -1) {
+        console.log("Detected: Safari");
         return "safari";
     }
     
     // 기본값
+    console.log("Detected: Safari (default)");
     return "safari";
 }
 
-// 브라우저에 따라 이미지 표시
 const browser = detectBrowser();
-console.log("Detected browser:", browser);
+console.log("Final browser:", browser);
 
 // body에 브라우저 클래스 추가
 document.body.classList.add(browser);
